5c45c683aed3ecda6bb82e7d394aeda952d8505d
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2024-04-18 18:23:00 +0200

    Sort utility classes.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-12 w-full rounded-md border border-black bg-secondary px-3 py-1 text-white text-sm lg:text-base shadow-sm transition-colors placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-12 w-full rounded-md border border-black bg-secondary px-3 py-1 text-sm text-white shadow-sm transition-colors placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50 lg:text-base",
           className
         )}
         ref={ref}

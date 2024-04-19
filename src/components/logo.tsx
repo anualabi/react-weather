20bd3e83a78458cc341bc92b6d51c90350386d17
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2024-04-19 23:43:48 +0200

    Sort utility classes.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import { H2 } from "@/components/ui";
 export default function Logo() {
   return (
     <Link href="/">
-      <H2 className="text-white font-black capitalize">React Weather</H2>
+      <H2 className="font-black capitalize text-white">React Weather</H2>
     </Link>
   );
 }

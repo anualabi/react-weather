a2013fc5ec3d38850752531c8604a0f32e330ec4
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2024-04-21 23:49:33 +0200

    Sort utility classes.

--- src/components/no-city-found.tsx
+++ src/components/no-city-found.tsx
@@ -4,11 +4,11 @@ import { Container, P } from "@/components/ui";
 
 export default function NoCityFound() {
   return (
-    <Container className="h-full flex flex-col items-center justify-center my-10">
-      <P className="text-white font-black text-lg mb-2">City not found.</P>
+    <Container className="my-10 flex h-full flex-col items-center justify-center">
+      <P className="mb-2 text-lg font-black text-white">City not found.</P>
       <Link
         href="/"
-        className="px-4 py-2 my-2 bg-secondary text-white rounded-md shadow-sm cursor-pointer"
+        className="my-2 cursor-pointer rounded-md bg-secondary px-4 py-2 text-white shadow-sm"
       >
         Go back to search
       </Link>

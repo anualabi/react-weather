e7c419f0e78557997b82553c3317faa5101748c2
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2024-04-22 23:51:50 +0200

    Sort utility classes.

--- src/app/(chart-section)/daily-evolution-loading.tsx
+++ src/app/(chart-section)/daily-evolution-loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui";
 export default function DailyEvolutionLoading() {
   return (
     <div className="p-4">
-      <Skeleton className="w-full h-64 rounded-md bg-zinc-900/10" />
+      <Skeleton className="h-64 w-full rounded-md bg-zinc-900/10" />
     </div>
   );
 }
